d25ee7ba92113a9be7d914cd0ff60e090d53f28f
Kevin Bastien <kevinb@in-mb13-nyc-041.ny.rga.com>
2015-06-16 17:48:00 -0700

    First commit with onboarding screen

---
+++ Forerunner/SelectDistanceViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Forerunner
+//
+//  Created by Kevin Bastien on 6/16/15.
+//  Copyright (c) 2015 Kevin Bastien. All rights reserved.
+//
+
+import UIKit
+
+class SelectDistanceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
