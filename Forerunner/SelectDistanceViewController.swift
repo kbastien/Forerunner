93feff9ec9ce1e7e9a88d3ced6ac79895d1dd154
Kevin Bastien <kevinb@in-mb13-nyc-041.ny.rga.com>
2015-06-17 19:09:17 -0700

    get current location works, now have to figure out how to access throughout the app

--- Forerunner/SelectDistanceViewController.swift
+++ Forerunner/SelectDistanceViewController.swift
@@ -7,17 +7,91 @@
 //
 
 import UIKit
+import CoreLocation
 
-class SelectDistanceViewController: UIViewController {
+class SelectDistanceViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var distancePicker: UIPickerView!
+    let pickerViewData = Array(0...50)
+    let pickerViewRows = 10000
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        distancePicker.dataSource = self
+        distancePicker.delegate = self
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+
+    func valueForRow(row: Int) -> Int {
+        // the rows repeat every `pickerViewData.count` items
+        return pickerViewData[row % pickerViewData.count]
+    }
+    
+    func rowForValue(value: Int) -> Int? {
+        let pickerViewMiddle = ((pickerViewRows / pickerViewData.count) / 2) * pickerViewData.count
+        if let valueIndex = find(pickerViewData, value) {
+            return pickerViewMiddle + value
+        }
+        return nil
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return "\(valueForRow(row))"
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerViewRows
+    }
+    
+    // whenever the picker view comes to rest, we'll jump back to
+    // the row with the current value that is closest to the middle
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let pickerViewMiddle = ((pickerViewRows / pickerViewData.count) / 2) * pickerViewData.count
+        let newRow = pickerViewMiddle + (row % pickerViewData.count)
+        pickerView.selectRow(newRow, inComponent: 0, animated: false)
+        println("Resetting row to \(newRow)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: { (placemarks, error) -> Void in
+            if error != nil {
+                println("Error: " + error.localizedDescription)
+                return
+            }
+            
+            if placemarks.count > 0 {
+                let pm = placemarks[0] as! CLPlacemark
+                self.displayLocationInfo(pm)
+            }
+            
+        })
+    }
+    
+    func displayLocationInfo(placemark: CLPlacemark){
+        self.locationManager.stopUpdatingLocation()
+        NSLog("Locality: \(placemark.locality)")
+        NSLog("Postal Code: %d \(placemark.postalCode)")
+        NSLog("Admin Area: \(placemark.administrativeArea)")
+        NSLog("Country: \(placemark.country)")
+    }
+    
+    func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error: " + error.localizedDescription)
     }
 
 }
--- Forerunner/SelectTagViewController.swift
+++ Forerunner/SelectTagViewController.swift
@@ -1,24 +1,24 @@
 //
-//  ViewController.swift
+//  SelectTagViewController.swift
 //  Forerunner
 //
-//  Created by Kevin Bastien on 6/16/15.
+//  Created by Kevin Bastien on 6/17/15.
 //  Copyright (c) 2015 Kevin Bastien. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SelectTagViewController: UIViewController {
+    @IBOutlet weak var currentLocationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentLocationLabel.text = "Portland, OR"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
-
